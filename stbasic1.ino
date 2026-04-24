fdf9524d088ab45ed70f39fcd68a580b71b98f61
Andrej Ristic <andrejnristic@gmail.com>
2026-04-24 11:53:03 +0200

    Add files via upload
    First, test code for the solar tracker basic task. Will be updated and improved soon.

---
+++ stbasic1.ino
@@ -0,0 +1,46 @@
+#include <Servo.h>
+int ldr1Pin = A0;  // LDR 1 (Levi senzor)
+int ldr2Pin = A1;  // LDR 2 (Desni senzor)
+int servoPin = 8;  // Servo motor pin
+Servo myservo;     // Pravi servo objekat
+int ldr1Value = 0;
+int ldr2Value = 0; 
+int servoPos = 90; // Pocetna pozicija (moze se menjati ali utice na if funkciju
+void setup() {
+  myservo.attach(servoPin); // Prikljuci servo pinu 8
+  pinMode(ldr1Pin, INPUT);  // LDR1 ulaz
+  pinMode(ldr2Pin, INPUT);  // LDR2 izlaz 
+  myservo.write(servoPos);  // Pocetna pozicija serva
+  Serial.begin(9600);       // Serijska kom, upali serial monitor 
+}
+int threshold = 20; // tolerancija
+
+void loop() {
+  ldr1Value = analogRead(ldr1Pin);
+  ldr2Value = analogRead(ldr2Pin);
+
+  int diff = ldr1Value - ldr2Value;
+
+  Serial.print("LDR1: ");
+  Serial.print(ldr1Value);
+  Serial.print(" LDR2: ");
+  Serial.println(ldr2Value);
+
+  if (abs(diff) > threshold) {
+    if (diff < 0) {
+      //ako je LDR2 jači pomeri panel udesno
+      if (servoPos < 180) { // moze se smanjiti ugao za 5 do 10 stepeni if (servoPos < 170) ...
+        servoPos += 1;
+        myservo.write(servoPos);
+      }
+    } else {
+      // ako je LDR1 jači pomeri panel u levo
+      if (servoPos > 0) { //if (servoPos > 10) ...
+        servoPos -= 1;
+        myservo.write(servoPos);
+      }
+    }
+  }
+
+  delay(50);
+}
